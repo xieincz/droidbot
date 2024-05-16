608c523f5164962c66ea0b6e965782854331b2f4
xieincz <39424496+xieincz@users.noreply.github.com>
2024-05-16 15:24:49 +0800

    debug

--- run.py
+++ run.py
@@ -43,15 +43,18 @@ def run_droidbot_v1(apk_path: str, output_dir_path: str, device_serial: str):
 
 def run_droidbot(apk_path: str, output_dir_path: str, device_serial: str):
     assert os.path.exists(apk_path), "APK file not found"
-    command = f"droidbot -keep_env -grant_perm -ignore_ad -count 18000 -a {apk_path} -o {output_dir_path} -d {device_serial}"
-    process = sp.Popen(command, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
+    run=["droidbot", "-keep_env", "-grant_perm", "-ignore_ad", "-count", "18000", "-a", apk_path, "-o", output_dir_path, "-d", device_serial]
+    process = sp.Popen(run, stdout=sp.PIPE, stderr=sp.STDOUT)
     # 实时打印输出
     while True:
         output = process.stdout.readline()
         if output == b"" and process.poll() is not None:
             break
         if output:
-            print(output.strip().decode("utf-8"))
+            try:
+                print(output.strip().decode("utf-8"))
+            except UnicodeDecodeError:
+                print(output)
     rc = process.poll()
     return rc == 0
 
